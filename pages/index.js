931ea0de98b307285b627c60ecc67b000d9635d3
Jhersy <jhersyvb@gmail.com>
2022-02-22 10:40:18 -0500

    Add my profile image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
 
 function Page() {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello, I&apos;am a full-stack developer based in Perú!
       </Box>
 
@@ -14,6 +26,19 @@ function Page() {
           </Heading>
           <p>Designer / FrontEnd / BackEnd</p>
         </Box>
+      </Box>
+
+      <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
+        <Image
+          src="/images/jhersy.jpg"
+          borderColor="whiteAlpha.800"
+          borderWidth={2}
+          borderStyle="solid"
+          maxWidth="100px"
+          display="inline-block"
+          borderRadius="full"
+          alt="Profile Image"
+        />
       </Box>
     </Container>
   )
