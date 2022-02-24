dda7e73e6dfffaf35ad0ccd5fc8e18439b851a94
Jhersy <jhersyvb@gmail.com>
2022-02-24 11:41:28 -0500

    Animate the toggle theme button

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,11 @@ import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import theme from '../lib/theme'
 
+// https://github.com/ant-design/ant-design/issues/30396#issuecomment-927299855
+import React from 'react'
+
+React.useLayoutEffect = React.useEffect
+
 export default class Document extends NextDocument {
   render() {
     return (
