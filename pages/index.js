031113b2a01e95f90e650928549858ce02168757
Jhersy <jhersyvb@gmail.com>
2022-02-24 20:15:31 -0500

    Implement page transitions with Framer Motion

--- pages/index.js
+++ pages/index.js
@@ -12,93 +12,92 @@ import NextLink from 'next/link'
 import { BioSection, BioYear } from '../components/bio'
 import Paragraph from '../components/paragraph'
 import Section from '../components/section'
+import Layout from '../components/layouts/article'
 
 function Page() {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        align="center"
-      >
-        Hello, I&apos;am a full-stack developer based in Perú!
-      </Box>
-
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Jhersy Developer
-          </Heading>
-          <p>Designer / FrontEnd / BackEnd</p>
-        </Box>
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
           align="center"
         >
-          <Image
-            src="/images/jhersy.jpg"
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            alt="Profile Image"
-          />
+          Hello, I&apos;am a full-stack developer based in Perú!
         </Box>
-      </Box>
-
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph>
-          Jhersy is a FullStack developer with passion for building apps and
-          digital services{' '}
-          <NextLink href="https://github.com/jhersyvb" passHref>
-            <Link>Github</Link>
-          </NextLink>
-          .
-        </Paragraph>
-        <Box align="center" my={4}>
-          <NextLink href="/works" passHref>
-            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-              My Portfolio
-            </Button>
-          </NextLink>
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Jhersy Developer
+            </Heading>
+            <p>Designer / FrontEnd / BackEnd</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
+          >
+            <Image
+              src="/images/jhersy.jpg"
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              alt="Profile Image"
+            />
+          </Box>
         </Box>
-      </Section>
-
-      <Section delay={0.2}>
-        <Heading as="h3" variant="section-title">
-          Bio
-        </Heading>
-        <BioSection>
-          <BioYear>1990</BioYear>
-          Born in Huánuco, Perú.
-        </BioSection>
-        <BioSection>
-          <BioYear>2012</BioYear>
-          Completed the program Systems Engineering in Hermilio Valdizan
-          University.
-        </BioSection>
-        <BioSection>
-          <BioYear>2016-present</BioYear>
-          Works at NOVA SYSTEM
-        </BioSection>
-      </Section>
-
-      <Section delay={0.3}>
-        <Heading as="h3" variant="section-title">
-          I ♥
-        </Heading>
-        <Paragraph>Drawing, Music and Watching series and movies</Paragraph>
-      </Section>
-    </Container>
+        <Section delay={0.1}>
+          <Heading as="h3" variant="section-title">
+            Work
+          </Heading>
+          <Paragraph>
+            Jhersy is a FullStack developer with passion for building apps and
+            digital services{' '}
+            <NextLink href="https://github.com/jhersyvb" passHref>
+              <Link>Github</Link>
+            </NextLink>
+            .
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works" passHref>
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                My Portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+        <Section delay={0.2}>
+          <Heading as="h3" variant="section-title">
+            Bio
+          </Heading>
+          <BioSection>
+            <BioYear>1990</BioYear>
+            Born in Huánuco, Perú.
+          </BioSection>
+          <BioSection>
+            <BioYear>2012</BioYear>
+            Completed the program Systems Engineering in Hermilio Valdizan
+            University.
+          </BioSection>
+          <BioSection>
+            <BioYear>2016-present</BioYear>
+            Works at NOVA SYSTEM
+          </BioSection>
+        </Section>
+        <Section delay={0.3}>
+          <Heading as="h3" variant="section-title">
+            I ♥
+          </Heading>
+          <Paragraph>Drawing, Music and Watching series and movies</Paragraph>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -1,38 +1,40 @@
+import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { Container, Box, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import { WorkGridItem } from '../components/grid-item'
 import thumbTwitterClone from '../public/images/works/twitter-clone.png'
 import thumbLinkedInClone from '../public/images/works/linkedin-clone.png'
 
 const Works = () => {
   return (
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
-
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <WorkGridItem
-            id="twitter-clone"
-            title="Twitter Clone"
-            thumbnail={thumbTwitterClone}
-          >
-            Build using Next.js, Tailwind CSS, Firebase v9, NextAuth, Recoil
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem
-            id="linkedin-clone"
-            title="LinkedIn Clone"
-            thumbnail={thumbLinkedInClone}
-          >
-            Build using Next.js 12, Middleware, Tailwind CSS, MongoDB, NextAuth,
-            Recoil
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-    </Container>
+    <Layout>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem
+              id="twitter-clone"
+              title="Twitter Clone"
+              thumbnail={thumbTwitterClone}
+            >
+              Build using Next.js, Tailwind CSS, Firebase v9, NextAuth, Recoil
+            </WorkGridItem>
+          </Section>
+          <Section>
+            <WorkGridItem
+              id="linkedin-clone"
+              title="LinkedIn Clone"
+              thumbnail={thumbLinkedInClone}
+            >
+              Build using Next.js 12, Middleware, Tailwind CSS, MongoDB,
+              NextAuth, Recoil
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
   )
 }
 
